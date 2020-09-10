379af686430ddc3fdb5cd42194ac9445cde626e9
Brian <bweloba@gmail.com>
2020-09-10 18:37:53 +0300

    add card functionality

--- js/work.js
+++ js/work.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
-    $("button.register").click(function (event) {
-        event.preventDefault();//prevents erasing of info after submitting
-        let name = $("#name").val();//gets name input from user
+$(document).ready(function() {
+    $("button.register").click(function(event) {
+        event.preventDefault(); //prevents erasing of info after submitting
+        let name = $("#name").val(); //gets name input from user
         // let email = $("#email").val(); //gets email input from user
         let category = $("#category").val(); //gets category input from user
         // let jobdetails = $("#jobdetails").val(); //gets message input from user
@@ -16,23 +16,17 @@ $(document).ready(function () {
         //shows various cards of category.
         //showing  Client's details of work.
         if (category === "Electrician") {
-            $("#cards1").show();
-            $("#workerName").append(name);
+            createDiv(1, "../images/pexels-led-supermarket-577513.jpg", $("#category").val(), $("#name").val());
         } else if (category === "Carpenter") {
-            $("#cards2").show();
-            $("#workerName2").append(name);
+            createDiv(2, "../images/pexels-magda-ehlers-2660262.jpg", $("#category").val(), $("#name").val());
         } else if (category === "Content creator") {
-            $("#cards3").show();
-            $("#workerName3").append(name);
+            createDiv(3, "../images/pexels-roman-odintsov-4553612.jpg", $("#category").val(), $("#name").val());
         } else if (category === "Plumber") {
-            $("#cards4").show();
-            $("#workerName4").append(name);
+            createDiv(4, "../images/pexels-pixabay-357440.jpg", $("#category").val(), $("#name").val());
         } else if (category === "Teacher") {
-            $("#cards5").show();
-            $("#workerName5").append(name);
+            createDiv(5, "../images/pexels-pixabay-301926.jpg", $("#category").val(), $("#name").val());
         } else if (category === "Other") {
-            $("#cards6").show();
-            $("#workerName6").append(name);
+            createDiv(6, "../images/pexels-ketut-subiyanto-4560150.jpg", $("#category").val(), $("#name").val());
         } else {
             alert("Choose a category!");
         }
@@ -40,25 +34,24 @@ $(document).ready(function () {
 });
 
 
-function register () {
-    var workers = [
-    {
+
+function register() {
+    var workers = [{
         workerName: $("#name").val(),
-        workerJob:  $("#category").val()
-    }
-    ];
+        workerJob: $("#category").val()
+    }];
 
     let workers_serialized = JSON.stringify(workers);
-    
+
 
     localStorage.setItem("workers", workers_serialized);
-    
+
     let workers_deserialized = JSON.parse(localStorage.getItem("workers"));
 
     for (var i = 0; i < workers_deserialized.length; i++) {
-    var output = '';      
-    output = workers[i].workerName + ":" + workers[i].workerJob;
-    myFunction(output);
+        var output = '';
+        output = workers[i].workerName + ":" + workers[i].workerJob;
+        myFunction(output);
     };
 
     // document.getElementById('preview').innerHTML = output;
@@ -74,3 +67,33 @@ function register () {
 
 };
 
+//method to create new div
+function createDiv(i, src, cat, nme) {
+    var container = document.getElementById("cardcont");
+    var el = document.createElement("div");
+    el.className = "card";
+    el.id = "cards" + i;
+    container.appendChild(el);
+    var img = document.createElement("IMG");
+    img.setAttribute("src", src);
+    img.className = "card-img-top";
+    el.appendChild(img);
+    var cBody = document.createElement("div");
+    cBody.className = "card-body";
+    el.appendChild(cBody);
+    var h5 = document.createElement("h5");
+    var p = document.createElement("p");
+    var hire = document.createElement("a");
+    h5.className = "card-text";
+    h5.id = "workerName";
+    h5.innerHTML = cat;
+    p.className = "card-title";
+    p.innerHTML = nme;
+    hire.href = "#";
+    hire.className = "btn";
+    hire.textContent = "Hire";
+    hire.classList.add("btn-primary");
+    el.appendChild(h5);
+    el.appendChild(p);
+    el.appendChild(hire);
+}
